e372ef4d3009d5a381b17af98c1fde2ea4a91148
Gerardo Garcia Carrizosa <56202397+gerardocarrizosa@users.noreply.github.com>
2021-05-31 21:23:48 -0700

    Add files via upload

---
+++ Cuenta de Ahorro/CN_Clientes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuenta_de_Ahorro
+{
+    class CN_Clientes
+    {
+    }
+}
---
+++ Cuenta de Ahorro/Connection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Cuenta_de_Ahorro
+{
+    class Connection
+    {
+        private SqlConnection Conexion = new SqlConnection("Server=DESKTOP-UEPK13H\\RONETJOHN;DataBase= Practica;Integrated Security=true");
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+    }
+}
